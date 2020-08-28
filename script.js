6867384d990458675dbe33dcf6335a8a062c8b0e
Parker Manning <parkerlmanning@hotmail.com>
2020-08-27 20:03:28 -0500

    boiler plate, correct jquery link, getting response from fitness calculator api

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // api key b469cb6cf6msh3da406e4c4c611dp13d77fjsnc9f693a4e222
+var age = 0;
+var height = 0;
+var weight = 0;
 
-var settings = {
+var fitnessCal = {
 	"async": true,
 	"crossDomain": true,
 	"url": "https://fitness-calculator.p.rapidapi.com/bmi?age=25&height=180&weight=65",
@@ -11,6 +14,6 @@ var settings = {
 	}
 }
 
-$.ajax(settings).done(function (response) {
+$.ajax(fitnessCal).done(function (response) {
 	console.log(response);
 });
