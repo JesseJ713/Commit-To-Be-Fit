2e2b20877224c251dfbed0597ffe873ea5e4ccf0
Parker Manning <parkerlmanning@hotmail.com>
2020-09-02 04:49:23 -0500

    Working Side-Nav Bar, Holds Favicon/link to our GitHub and linkedIn

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 $(document).ready(function () {
+//implementing 3rd party Framework
+function contactUs() {
+  var x = document.getElementById("demoAcc");
+  if (x.className.indexOf("w3-show") == -1) {
+    x.className += " w3-show";
+  } else {
+    x.className = x.className.replace(" w3-show", "");
+  }
+}
+
+// Click on the "Jeans" link on page load to open the accordion for demo purposes
+document.getElementById("myBtn").click();
+
+
+// Open and close sidebar
+function w3_open() {
+  document.getElementById("mySidebar").style.display = "block";
+  document.getElementById("myOverlay").style.display = "block";
+}
+ 
+function w3_close() {
+  document.getElementById("mySidebar").style.display = "none";
+  document.getElementById("myOverlay").style.display = "none";
+}
 // function for creating a dropdown menu with a list of ages with a corresponding value
 createAgeList();
 function createAgeList() {
